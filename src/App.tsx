51ae8028cf8d6562dbfc58c930d2b3709c0e7c17
Woo <jiwoopark727@gmail.com>
2024-08-22 15:26:46 +0900

    조건부 렌더링 if문, 삼항연산자(?)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import Button from './html/Button'
-import Input from './html/Input'
-import CheckBox from './html/CheckBox'
+// import Button from './html/Button'
+// import Input from './html/Input'
+// import CheckBox from './html/CheckBox'
+
+// const App = () => {
+//   return (
+//     <>
+//       <div className='item-middle flex-col gap-4'>
+//         <Button type='button' className='bg-[#4f4f4f]' name='add-btn' disabled={false}>Add</Button>
+//         <Button type='button' className='bg-[#e13333]' name='cancel-btn' disabled={false}>Cancel</Button>
+//         <Button type='button' className='bg-[#8d196c]' name='success-btn' disabled={false}>Success</Button>
+
+//         <Input type='text' placeholder='Enter your name'/>
+//         <Input type='password' placeholder='Enter your name'/>
+//         <Input type='number' placeholder='Enter your name'/>
+//         <div className='flex items-center gap-2'>
+//           <CheckBox type="checkbox">I agree with <strong>terms</strong> and <strong>plicies</strong>.</CheckBox>
+//           <CheckBox type="checkbox">I agree with <strong>terms</strong> and <strong>plicies</strong>.</CheckBox>
+
+//         </div>
+//       </div>  
+//     </>
+//   )
+// }
+// export default App;
 
 const App = () => {
+  const isLogin = true;
+
   return (
     <>
-      <div className='item-middle flex-col gap-4'>
-        <Button type='button' className='bg-[#4f4f4f]' name='add-btn' disabled={false}>Add</Button>
-        <Button type='button' className='bg-[#e13333]' name='cancel-btn' disabled={false}>Cancel</Button>
-        <Button type='button' className='bg-[#8d196c]' name='success-btn' disabled={false}>Success</Button>
-
-        <Input type='text' placeholder='Enter your name'/>
-        <Input type='password' placeholder='Enter your name'/>
-        <Input type='number' placeholder='Enter your name'/>
-        <div className='flex items-center gap-2'>
-          <CheckBox type="checkbox">I agree with <strong>terms</strong> and <strong>plicies</strong>.</CheckBox>
-          <CheckBox type="checkbox">I agree with <strong>terms</strong> and <strong>plicies</strong>.</CheckBox>
-
-        </div>
-      </div>  
+      {/* {isLogin ? <h1>Logged In</h1> : <h1>Not Logged In</h1>}
+      <h1>{isLogin ? "Logged In" : "Not Logged In"}</h1> */}
     </>
   )
 }
