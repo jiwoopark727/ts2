fa31f3ecf1c2c164da28f0855bf6f676a29162f4
Woo <jiwoopark727@gmail.com>
2024-08-22 13:42:16 +0900

    input 재사용성 컴포넌트화-2, 디자인 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import CheckBox from './html/CheckBox'
 const App = () => {
   return (
     <>
-      <div className='item-middle'>
-        {/* <Button type='button' className='bg-[#4f4f4f]' name='add-btn' disabled={false}>Add</Button>
+      <div className='item-middle flex-col gap-4'>
+        <Button type='button' className='bg-[#4f4f4f]' name='add-btn' disabled={false}>Add</Button>
         <Button type='button' className='bg-[#e13333]' name='cancel-btn' disabled={false}>Cancel</Button>
-        <Button type='button' className='bg-[#8d196c]' name='success-btn' disabled={false}>Success</Button> */}
+        <Button type='button' className='bg-[#8d196c]' name='success-btn' disabled={false}>Success</Button>
 
         <Input type='text' placeholder='Enter your name'/>
+        <Input type='password' placeholder='Enter your name'/>
+        <Input type='number' placeholder='Enter your name'/>
       </div>  
     </>
   )
--- src/html/Input.tsx
+++ src/html/Input.tsx
@@ -5,7 +5,7 @@ const Input = (props: TInputProps) => {
   return (
     <>
       <input 
-        className='inter text-sm border border-[#4f4f4f] py-[13.5px] px-[16px] 
+        className='inter h-[44px] text-sm border border-[#4f4f4f] py-[13.5px] px-[16px] 
         rounded-lg placeholder-[#acacac]'
         {...rest}
       />
