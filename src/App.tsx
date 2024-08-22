83c993e27f14d71969cbb415115f2a4d53c31e75
Woo <jiwoopark727@gmail.com>
2024-08-22 15:54:36 +0900

    map함수 이용 반복(배열에 가능)

--- src/App.tsx
+++ src/App.tsx
@@ -24,13 +24,29 @@
 // }
 // export default App;
 
+// const App = () => {
+//   const isLogin = true;
+
+//   return (
+//     <>
+//       {/* {isLogin ? <h1>Logged In</h1> : <h1>Not Logged In</h1>}
+//       <h1>{isLogin ? "Logged In" : "Not Logged In"}</h1> */}
+//     </>
+//   )
+// }
+// export default App;
+
 const App = () => {
-  const isLogin = true;
+  const fruits =  ["사과", "바나나", "체리"];
 
   return (
     <>
-      {/* {isLogin ? <h1>Logged In</h1> : <h1>Not Logged In</h1>}
-      <h1>{isLogin ? "Logged In" : "Not Logged In"}</h1> */}
+      <h1>내가 좋아하는 과일은</h1>
+      <ul>
+        {fruits.map((item, i)=>(
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
     </>
   )
 }
